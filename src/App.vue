3921418c1b128b0db4bcea728f6c98080fe53267
Paulo dos Santos <paulo@fazendadosoftware.com>
2019-10-12 17:34:06 -0300

    curretn state

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="h-screen w-screen flex bg-gray-200">
+      {{language}}
+      <div class="w-1/2 text-gray-700 text-center bg-gray-400 p-2 m-2 flex items-center justify-center">
+        <textarea v-model="text" class="w-full h-full border rounded focus:outline-none"/>
+      </div>
+      <div class="w-1/2 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2 flex items-center justify-center">
+        <div class="hyphenate resize overflow-auto m-2 px-4 py-2 bg-white shadow">{{text}}</div>
+        <div class="hyphenate resize overflow-auto m-2 px-4 py-2 bg-white shadow bg-yellow-300">
+          {{$hyphenate.de(text)}}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data: () => ({
+    text: '',
+    language: ''
+  }),
+  watch: {
+    text (val) {
+      this.language = this.$franc(val, { only: ['por', 'spa', 'eng', 'deu', 'fra'] })
+    }
+  },
+  created () {
+    this.$lx.init()
+      .then(reportSetup => {
+        this.$lx.ready({})
+      })
   }
 }
 </script>
+
 <style lang="stylus">
 #app
   font-family 'Avenir', Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+
+.hyphenate
+  overflow-wrap break-word
+  word-wrap break-word
+  hyphens auto
+  -webkit-hyphens auto
+  -ms-hyphens auto
+  // hyphenate-limit-chars 10 4 4
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import '@leanix/reporting'
+import './assets/css/main.css'
+import franc from 'franc'
+import hyphen from 'hyphen'
+import dePattern from 'hyphen/patterns/de'
+import enUSPattern from 'hyphen/patterns/en-us'
 
 /* global lx */
 Vue.prototype.$lx = lx
+Vue.prototype.$franc = franc
+
+Vue.prototype.$hyphenate = {
+  de: hyphen(dePattern, { hyphenChar: '-' }),
+  'en-us': hyphen(enUSPattern, { hyphenChar: '-' })
+}
 
 Vue.config.productionTip = false
 
