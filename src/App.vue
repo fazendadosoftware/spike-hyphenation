0b53cb2812a11ea86cb7e7424ac2ba550bb0bdc4
Paulo dos Santos <paulo@fazendadosoftware.com>
2019-10-14 08:34:38 -0300

    current state

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
   <div id="app">
     <div class="h-screen w-screen flex bg-gray-200">
-      {{language}}
-      <div class="w-1/2 text-gray-700 text-center bg-gray-400 p-2 m-2 flex items-center justify-center">
-        <textarea v-model="text" class="w-full h-full border rounded focus:outline-none"/>
+      <div class="w-1/2 text-gray-700 text-center bg-gray-400 p-2 m-2 flex flex-col items-center justify-center">
+        <textarea v-model="text" class="w-full h-full border rounded focus:outline-none p-2"/>
+        <div class="flex flex-col rounded p-2 m-2 mb-0 bg-white shadow items-center">
+          <flag v-if="language.icon" :iso="language.icon" :squared="false" class="text-5xl shadow"/>
+          <div class="text-5xl font-semibold" v-else>?</div>
+          <span class="font-semibold mt-2">{{language.label}}</span>
+        </div>
       </div>
-      <div class="w-1/2 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2 flex items-center justify-center">
+      <div class="w-1/2 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2 flex flex-col items-center justify-center">
         <div class="hyphenate resize overflow-auto m-2 px-4 py-2 bg-white shadow">{{text}}</div>
-        <div class="hyphenate resize overflow-auto m-2 px-4 py-2 bg-white shadow bg-yellow-300">
-          {{$hyphenate.de(text)}}
+        <div class="w-1/2 resize overflow-auto bg-white rounded shadow p-2">
+          <div class="flex border-b mb-2 justify-between items-center cursor-pointer">
+            <span class="font-bold text-md mx-4">hyphen</span>
+            <img class="m-3" width="30" src="./assets/img/github.svg"/>
+          </div>
+          <div class="hyphenate mt-2 hyphens-manual bg-red-100 flex-1">
+            {{hyphenTranslated}}
+          </div>
         </div>
       </div>
     </div>
@@ -21,11 +31,30 @@ export default {
   name: 'app',
   data: () => ({
     text: '',
-    language: ''
+    detectedLanguage: 'und',
+    languages: {
+      por: { label: 'Portuguese', icon: 'pt' },
+      spa: { label: 'Spanish', icon: 'es' },
+      eng: { label: 'English', icon: 'us' },
+      deu: { label: 'German', icon: 'de' },
+      fra: { label: 'French', icon: 'fr' },
+      ita: { label: 'Italian', icon: 'it' },
+      und: { label: 'Language undefined' }
+    }
   }),
+  computed: {
+    language () {
+      return this.languages[this.detectedLanguage]
+    },
+    hyphenTranslated () {
+      const hyphenator = this.$hyphenate[this.detectedLanguage]
+      console.log('hyphenator', hyphenator)
+      return hyphenator ? hyphenator(this.text) : this.text
+    }
+  },
   watch: {
     text (val) {
-      this.language = this.$franc(val, { only: ['por', 'spa', 'eng', 'deu', 'fra'] })
+      this.detectedLanguage = this.$franc(val, { only: ['por', 'spa', 'eng', 'deu', 'fra', 'ita'] })
     }
   },
   created () {
@@ -50,4 +79,13 @@ export default {
   -webkit-hyphens auto
   -ms-hyphens auto
   // hyphenate-limit-chars 10 4 4
+
+.hyphens-none
+  hyphens none
+
+.hyphens-manual
+  hyphens manual
+
+.hyphens-auto
+  hyphens auto
 </style>
--- src/main.js
+++ src/main.js
@@ -1,20 +1,32 @@
 import Vue from 'vue'
 import App from './App.vue'
+import FlagIcon from 'vue-flag-icon'
 import '@leanix/reporting'
 import './assets/css/main.css'
 import franc from 'franc'
 import hyphen from 'hyphen'
 import dePattern from 'hyphen/patterns/de'
 import enUSPattern from 'hyphen/patterns/en-us'
+import ptPattern from 'hyphen/patterns/pt'
+import esPattern from 'hyphen/patterns/es'
+import frPattern from 'hyphen/patterns/fr'
+import itPattern from 'hyphen/patterns/it'
 
 /* global lx */
 Vue.prototype.$lx = lx
+
 Vue.prototype.$franc = franc
 
 Vue.prototype.$hyphenate = {
-  de: hyphen(dePattern, { hyphenChar: '-' }),
-  'en-us': hyphen(enUSPattern, { hyphenChar: '-' })
+  deu: hyphen(dePattern),
+  eng: hyphen(enUSPattern),
+  por: hyphen(ptPattern),
+  spa: hyphen(esPattern),
+  fra: hyphen(frPattern),
+  ita: hyphen(itPattern)
 }
+
+Vue.use(FlagIcon)
 
 Vue.config.productionTip = false
 
